a009c6b10198db07def62529b3360cd40860acc8
Tri Chu <134897+tri-chu@users.noreply.github.com>
2023-06-15 11:35:50 -0700

    update keymap

--- nvim/after/plugin/telescrope.lua
+++ nvim/after/plugin/telescrope.lua
@@ -1,10 +1,10 @@
 local telescope = require('telescope')
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<C-space>', builtin.find_files, {})
-vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
-vim.keymap.set('n', '<C-b>', builtin.buffers, {})
-vim.keymap.set('n', '<C-c>', builtin.commands, {})
+vim.keymap.set('n', '<leader>t', builtin.find_files, {})
+vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+vim.keymap.set('n', '<leader>c', builtin.commands, {})
 
 telescope.load_extension('fzf')
 telescope.setup({
--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -1,63 +1,69 @@
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+    local fn = vim.fn
+    local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+    if fn.empty(fn.glob(install_path)) > 0 then
+        fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+        vim.cmd [[packadd packer.nvim]]
+        return true
+    end
+    return false
 end
 
 local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
-  -- Self manage packer
-  use 'wbthomason/packer.nvim'
+    -- Self manage packer
+    use 'wbthomason/packer.nvim'
 
-  -- Search modal
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
-    requires = {
-      { 'nvim-lua/plenary.nvim' },
-      { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+    -- Search modal
+    use {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.1',
+        requires = {{'nvim-lua/plenary.nvim'}, {
+            'nvim-telescope/telescope-fzf-native.nvim',
+            run = 'make'
+        }}
     }
-  }
 
-  -- Basic stuff
-  use 'tpope/vim-surround'
-  use 'tpope/vim-commentary'
-  use 'tpope/vim-repeat'
+    -- Basic stuff
+    use 'tpope/vim-surround'
+    use 'tpope/vim-commentary'
+    use 'tpope/vim-repeat'
 
-   -- Source Code Management
-   use 'mhinz/vim-signify'
-   use 'tpope/vim-fugitive'
+    -- Source Code Management
+    use 'mhinz/vim-signify'
+    use 'tpope/vim-fugitive'
 
-  ---------------
-  -- Aesthetics
-  ---------------
+    ---------------
+    -- Aesthetics
+    ---------------
 
-  -- Tree view
-  use {
-    'nvim-tree/nvim-tree.lua',
-    requires = {
-      'nvim-tree/nvim-web-devicons',
-    },
-  }
+    -- Tree view
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {'nvim-tree/nvim-web-devicons'},
+    }
 
-  use 'lukas-reineke/indent-blankline.nvim'
-  use { 'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons' }
+    use 'lukas-reineke/indent-blankline.nvim'
+    use {
+        'akinsho/bufferline.nvim',
+        tag = "v3.*",
+        requires = 'nvim-tree/nvim-web-devicons'
+    }
 
-  -- Theme
-  use 'Shatur/neovim-ayu'
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-  }
+    -- Theme
+    use 'Shatur/neovim-ayu'
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = {
+            'nvim-tree/nvim-web-devicons',
+            opt = true
+        }
+    }
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
