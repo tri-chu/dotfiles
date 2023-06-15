78ed1cd248cd76d89058e5e44564ed1780b2f53e
Tri Chu <134897+tri-chu@users.noreply.github.com>
2023-06-15 14:00:43 -0700

    update telescope

--- nvim/after/plugin/telescrope.lua
+++ nvim/after/plugin/telescrope.lua
@@ -1,21 +1,22 @@
 local telescope = require('telescope')
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>t', builtin.find_files, {})
-vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>b', builtin.buffers, {})
-vim.keymap.set('n', '<leader>c', builtin.commands, {})
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
+vim.keymap.set('n', '<C-b>', builtin.buffers, {})
+vim.keymap.set('n', '<C-c>', builtin.commands, {})
 
 telescope.load_extension('fzf')
 telescope.setup({
-  pickers = {
-    find_files = {
-      hidden = true
-    },
-    live_grep = {
-      additional_args = function(opts)
-        return { "--hidden" }
-      end
-    },
-  },
+    pickers = {
+        find_files = {
+            hidden = true,
+            file_ignore_patterns = {"^./.git/", "^node_modules/"}
+        },
+        live_grep = {
+            additional_args = function(opts)
+                return {"--hidden"}
+            end
+        }
+    }
 })
