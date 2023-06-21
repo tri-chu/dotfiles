7c550de392faf561955fa95a8be3bd419074c332
Tri Chu <134897+tri-chu@users.noreply.github.com>
2023-06-21 16:07:05 -0700

    update cursor behavior

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -48,6 +48,12 @@ opt.updatetime = 500 -- milliseconds
 opt.number = true
 
 -----------------------------------------------
+-- Cursor stuff
+-----------------------------------------------
+
+opt.guicursor = 'n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175'
+
+-----------------------------------------------
 -- Indent and space
 -----------------------------------------------
 -- Set indent to be 4 spaces
